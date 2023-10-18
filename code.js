4249ffa8ea4b2e8d1fe8a5073be7be477f38743a
Michael Ventura <m.ventura14@yahoo.com>
2023-10-18 16:59:20 -0500

    unit tests for edge cases.

--- code.js
+++ code.js
@@ -1,9 +1,14 @@
 // helloWorld function 
-const helloWorld = function() {
+const helloWorld = function () {
     return "Hello, World!";
 }
 
-function sayHello(input){
-    return "Hello, " + input + "!";
-
+function sayHello(input) {
+    if (!isNaN(input)) {
+        return 'Hello, World!'
+    } else if (typeof input === "string") {
+        return "Hello, " + input + "!";
+    } else {
+        return 'Hello, World!';
+    }
 }
--- tests.js
+++ tests.js
@@ -19,14 +19,38 @@ describe('sayHello', function () {
     it('should return a string when called', function () {
         expect(typeof sayHello()).toBe("string");
     })
-    it('should return a string "Hello, Jane!"', function () {
+    it('should return a string "Hello, Jane! when input is Jane"', function () {
         expect(sayHello("Jane")).toBe("Hello, Jane!");
     })
-    it('should return a string "Hello, Alex!"', function () {
+    it('should return a string "Hello, Alex!" when input is Alex', function () {
         expect(sayHello("Alex")).toBe("Hello, Alex!")
     })
-    it('should return a string "Hello, Pat!"', function () {
+    it('should return a string "Hello, Pat! when input is Pat"', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!")
     })
+    it('should return a string "Hello, World!" when executed', function () {
+        expect(sayHello()).toBe("Hello, World!")
+    })
+    it('should return a string "Hello, World!" when input is true', function () {
+        expect(sayHello(true)).toBe("Hello, World!")
+    })
+    it('should return a string "Hello, World!" when input is false', function () {
+        expect(sayHello(false)).toBe("Hello, World!")
+    })
+    it('should return a string "Hello, World!" when input is a number', function () {
+        expect(sayHello(23)).toBe("Hello, World!")
+    })
+    it('should return a string "Hello, World!" when input is a number as a string', function () {
+        expect(sayHello("23")).toBe("Hello, World!")
+    })
+    it('should return a string "Hello, World!" when input is an empty string', function () {
+        expect(sayHello("")).toBe("Hello, World!")
+    })
+    it('should return a string "Hello, World!" when input starts with a number', function () {
+        expect(sayHello(null)).toBe("Hello, World!")
+    })
+
+
+
 })
 
